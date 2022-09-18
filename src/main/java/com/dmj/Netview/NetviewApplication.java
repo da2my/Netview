832bd945f3959726f1d18d9136a42a9b4e17bda6
da2my <davo.da2m@gmail.com>
2022-09-18 19:24:45 +0200

    primer commit

---
+++ src/main/java/com/dmj/Netview/NetviewApplication.java
@@ -0,0 +1,13 @@
+package com.dmj.Netview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetviewApplication.class, args);
+	}
+
+}

deb4fc46e7b51469d95ed180cbec1ac75ee38866
MARCOS CANCHO QUIÑONES <marcos.cancho@gmail.com>
2022-10-03 21:11:47 +0200

    Add config FB

--- src/main/java/com/dmj/Netview/configuracion/Configuracion.java
+++
@@ -1,9 +0,0 @@
-package com.dmj.Netview.configuracion;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Configuracion {
-
-}
-
---
+++ src/main/java/com/dmj/Netview/configuracion/ConfiguracionFB.java
@@ -0,0 +1,41 @@
+package com.dmj.Netview.configuracion;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+@Configuration
+public class ConfiguracionFB {
+
+	@Bean
+	public Firestore inicializarFB() {
+		FileInputStream serviceAccount;
+
+		try {
+
+			serviceAccount = new FileInputStream(""); //FALTA AGREGAR RUTA
+
+			FirebaseOptions options = FirebaseOptions.builder()
+
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://netview-3cece.firebaseio.com").build();
+
+			FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
+
+			return FirestoreClient.getFirestore(firebaseApp);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}

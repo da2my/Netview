871c4ff995dd89e0d5b3991a9c3188b105312308
da2my <davo.da2m@gmail.com>
2022-11-09 20:09:48 +0100

    peticion de registro de usuario

--- src/main/java/com/dmj/Netview/controlador/UsuarioRegistroControlador.java
+++ src/main/java/com/dmj/Netview/controlador/UsuarioRegistroControlador.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.dmj.Netview.dto.UsuarioRegistroDTO;
 import com.dmj.Netview.repositorios.UsuarioRepositorio;
 
 @Controller
@@ -15,12 +16,12 @@ public class UsuarioRegistroControlador {
 		this.useRepository = useService;
 	}
 
-	public UsuarioRegistroDTO() {
+	public UsuarioRegistroDTO userRegistrationDTO() {
 		return new UsuarioRegistroDTO();
 	}
 
 	@PostMapping("/app/register")
-	public String registerUserAccount(@ModelAttribute UsuarioRegistroDTO registrationDTO) {
+	public String registerUserAccount(@ModelAttribute UsuarioRegistroDTO registrationDTO) throws Exception{
 		useRepository.guardar(registrationDTO);
 		return "redirect:/app/login?reg_OK";
 

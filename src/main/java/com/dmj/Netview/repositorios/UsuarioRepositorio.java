f82a432080fc0133ccc25d8e473519652972bb0c
da2my <davo.da2m@gmail.com>
2022-11-09 20:15:38 +0100

    añadir metodo guardar con ApiFuture para que escuche el pa peticion de registro

--- src/main/java/com/dmj/Netview/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/dmj/Netview/repositorios/UsuarioRepositorio.java
@@ -2,7 +2,8 @@ package com.dmj.Netview.repositorios;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Repository;
-import com.dmj.Netview.modelo.Usuario;
+
+import com.dmj.Netview.dto.UsuarioRegistroDTO;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 
@@ -10,6 +11,6 @@ import com.google.cloud.firestore.DocumentReference;
 @Repository
 public interface UsuarioRepositorio extends UserDetailsService {
 	
-	ApiFuture<DocumentReference> guardar(Usuario registrationUser) throws Exception;
+	ApiFuture<DocumentReference> guardar(UsuarioRegistroDTO registrationDTO) throws Exception;
 
 }

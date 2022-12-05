be57288720fa824151a022f55c4836405fc30ff9
da2my <davo.da2m@gmail.com>
2022-12-05 17:43:27 +0100

    Eliminado contenido target

--- target/classes/static/css/JS/main.js
+++
@@ -1,9 +0,0 @@
-const signUpA = document.getElementById('signUp');
-const signInA = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpA.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
-
-signInA.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));

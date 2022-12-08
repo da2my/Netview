35139e78f216029c7e78d1fafcd40af54c322960
MARCOS CANCHO QUIÑONES <marcos.cancho@gmail.com>
2022-12-08 16:48:53 +0100

    control contraseñas diferentes

--- src/main/resources/static/css/JS/main.js
+++ src/main/resources/static/css/JS/main.js
@@ -1,9 +1,63 @@
+
 const signUpA = document.getElementById('signUp');
 const signInA = document.getElementById('signIn');
 const container = document.getElementById('container');
+const parrafo = document.getElementById("warnings")
 
 signUpA.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
+    container.classList.add('right-panel-active'));
 
 signInA.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));
+    container.classList.remove('right-panel-active'));
+
+
+
+let nomb;
+let ape;
+let mail;
+let contrasena;
+let contrasenarep;
+
+
+$("input").change(function () {
+    nomb = document.getElementById("nomb").value;
+    ape = document.getElementById("ape").value;
+    mail = document.getElementById("mail").value;
+    contrasena = document.getElementById("contrasena").value;
+    contrasenarep = document.getElementById("contrasenarep").value;
+
+    console.log(nomb + ape + mail + contrasena + contrasenarep)
+
+    desa();
+});
+
+
+
+function over() {
+    if (contrasena != contrasenarep) {
+
+        warnings = 'LAS CONTRASEÑAS NO COINCIDEN <br>'
+        parrafo.innerHTML = warnings
+
+    }
+}
+
+function desa() {
+
+
+
+    if (nomb != "" && ape != "" && mail != "" && contrasena == contrasenarep && contrasenarep != "") {
+
+        console.log("El boton está activo (o debería)")
+        document.getElementById("btn-signup").disabled = false;
+
+    }
+
+    else {
+        console.log("El boton está full disabled")
+        document.getElementById("btn-signup").disabled = true;
+    }
+
+}
+
+

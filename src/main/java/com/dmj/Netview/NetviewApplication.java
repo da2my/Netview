39f47b6e3f059eb379ba69ad2c2e79d1bc03aeb2
Javier <erobleo@gmail.com>
2022-12-08 21:52:33 +0100

    update project

--- src/main/java/com/dmj/Netview/NetviewApplication.java
+++ src/main/java/com/dmj/Netview/NetviewApplication.java
@@ -9,28 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.dmj.Netview.servicios.StreamingService;
-
 import reactor.core.publisher.Mono;
 
 @Configuration  
 @RestController
 @SpringBootApplication
 public class NetviewApplication {
-
-	@Autowired
-    private StreamingService service;
 	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(NetviewApplication.class, args);
 	}
-	
-	 @GetMapping(value = "video/{title}", produces = "video/mp4")
-	    public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range) {
-	        System.out.println("range in bytes() : " + range);
-	        return service.getVideo(title);
-	    }
 
 }
